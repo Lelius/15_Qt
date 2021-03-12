4679ebf249e10a3c0da21c3d62ebd16a129d8d41
Lelius <leliuslogunov@gmail.com>
2021-03-12 16:09:49 +0200

    Загрузил все картинки в память по указателям QGraphicsPixmapItem *.

--- maingemewidget.cpp
+++ maingemewidget.cpp
@@ -12,10 +12,28 @@ MainGameWidget::MainGameWidget(QWidget *parent)
 
     scene = new QGraphicsScene(this);
     pixmapBox =  scene->addPixmap(QPixmap(":/image/box.png"));
-    pixmapBox->setOffset(QPointF(0, 0));
     pixmapRestartButton = scene->addPixmap(QPixmap(":/image/reset.png"));
-    pixmapRestartButton->setOffset(QPointF(0, 445));
     pixmapQuitButton = scene->addPixmap(QPixmap(":/image/X3.png"));
+    pixmap_1_chip = scene->addPixmap(QPixmap(":/image/1_chip.png"));
+    pixmap_2_chip = scene->addPixmap(QPixmap(":/image/2_chip.png"));
+    pixmap_3_chip = scene->addPixmap(QPixmap(":/image/3_chip.png"));
+    pixmap_4_chip = scene->addPixmap(QPixmap(":/image/4_chip.png"));
+    pixmap_5_chip = scene->addPixmap(QPixmap(":/image/5_chip.png"));
+    pixmap_6_chip = scene->addPixmap(QPixmap(":/image/6_chip.png"));
+    pixmap_7_chip = scene->addPixmap(QPixmap(":/image/7_chip.png"));
+    pixmap_8_chip = scene->addPixmap(QPixmap(":/image/8_chip.png"));
+    pixmap_9_chip = scene->addPixmap(QPixmap(":/image/9_chip.png"));
+    pixmap_10_chip = scene->addPixmap(QPixmap(":/image/10_chip.png"));
+    pixmap_11_chip = scene->addPixmap(QPixmap(":/image/11_chip.png"));
+    pixmap_12_chip = scene->addPixmap(QPixmap(":/image/12_chip.png"));
+    pixmap_13_chip = scene->addPixmap(QPixmap(":/image/13_chip.png"));
+    pixmap_14_chip = scene->addPixmap(QPixmap(":/image/14_chip.png"));
+    pixmap_15_chip = scene->addPixmap(QPixmap(":/image/15_chip.png"));
+    pixmapWellBlack = scene->addPixmap(QPixmap(":/image/well-black_380x350.png"));
+    pixmapWellWhite = scene->addPixmap(QPixmap(":/image/well-white_380x350.png"));
+
+    pixmapBox->setOffset(QPointF(0, 0));
+    pixmapRestartButton->setOffset(QPointF(0, 445));
     pixmapQuitButton->setOffset(QPointF(333, 445));
 
     ui->graphicsView->setScene(scene);
